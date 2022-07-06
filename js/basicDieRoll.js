341e2b3001c618379363c89c6fde609058ad3b3e
sockmonkeyy <mr.richardson718@gmail.com>
2022-07-06 12:40:21 -0500

    no changes

--- js/basicDieRoll.js
+++ js/basicDieRoll.js
@@ -1,5 +1,6 @@
 const die1 = Math.floor(Math.random() * 6) + 1; // This includes the basics of math.floor and Math.random.
-const die2 = Math.floor(Math.random() * 6) + 1; 
+const die2 = Math.floor(Math.random() * 6) + 1;
 
 
 let roll = `You rolled a ${die1} and a ${die2}. They sum to ${die1 + die2}` 
+console.log(roll)
