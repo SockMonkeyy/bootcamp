9e14fd61d27983f782c856eb0cd19b6eed70cbd0
sockmonkeyy <mr.richardson718@gmail.com>
2022-07-06 21:13:57 -0500

    July 6th Odin Practice

--- js/basicDieRoll.js
+++ js/basicDieRoll.js
@@ -3,4 +3,15 @@ const die2 = Math.floor(Math.random() * 6) + 1;
 
 
 let roll = `You rolled a ${die1} and a ${die2}. They sum to ${die1 + die2}` 
-console.log(roll)
+if (roll == 7 || roll == 11) {
+    console.log('You Win');
+}
+
+else if (roll == 2 || roll == 12) {
+
+    console.log('You Lose');
+}
+ else () {
+    console.log('Roll Again')
+
+}
---
+++ js/craps.js
@@ -0,0 +1,29 @@
+
+const die1 = Math.floor(Math.random() * 6) + 1;
+const die2 = Math.floor(Math.random() * 6) + 1;
+const rollOne = die1 + die2;
+const point = rollOne;
+
+if (rollOne == 7 || rollOne == 11) {
+   
+    console.log("You rolled a" + " " + rollOne + " " + "You Win!");
+} else if (rollOne == 2 || rollOne == 12) {
+    console.log("You rolled a" + " " + rollOne + " " + "You Lose");
+} else {
+    let roll2 = console.log("You rolled a" + " " + rollOne + " " + "roll again");
+}
+{
+else
+{
+
+if (rollOne == point) {
+    console.log("You rolled a" + " " + rollOne + " " + "You Win!")
+}
+else if (roll2 == 7) {
+    console.log("You rolled a" + " " + rollOne + " " + "You Lose")
+}
+else {
+    "You rolled a" + " " + roll2 + " " + "roll again"
+
+}
+
