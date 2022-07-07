0e7047d945bc7c030f148dde2b2d3cc8e36c0a8f
sockmonkeyy <mr.richardson718@gmail.com>
2022-07-07 07:57:46 -0500

    July 7th morning practice

--- js/craps.js
+++ js/craps.js
@@ -1,29 +1,19 @@
-
+const myBtn = document.querySelector('myBtn')
 const die1 = Math.floor(Math.random() * 6) + 1;
 const die2 = Math.floor(Math.random() * 6) + 1;
 const rollOne = die1 + die2;
 const point = rollOne;
 
+
+let = rollOne;
 if (rollOne == 7 || rollOne == 11) {
-   
-    console.log("You rolled a" + " " + rollOne + " " + "You Win!");
+    myBtn.addEventListener('Play Again', rollOne);
+    console.log("You rolled a `${rollOne}` You Win!");
 } else if (rollOne == 2 || rollOne == 12) {
-    console.log("You rolled a" + " " + rollOne + " " + "You Lose");
+    myBtn.addEventListener('Play Again', rollOne)
+    console.log("You rolled a " + rollOne + " You Lose");
 } else {
-    let roll2 = console.log("You rolled a" + " " + rollOne + " " + "roll again");
-}
-{
-else
-{
-
-if (rollOne == point) {
-    console.log("You rolled a" + " " + rollOne + " " + "You Win!")
-}
-else if (roll2 == 7) {
-    console.log("You rolled a" + " " + rollOne + " " + "You Lose")
-}
-else {
-    "You rolled a" + " " + roll2 + " " + "roll again"
-
+    myBtn.addEventListener('Roll Again', rollOne)
+    let roll2 = console.log("You rolled a " + rollOne + " roll again");
 }
 
--- js/practice.js
+++ js/practice.js
@@ -1,5 +1,10 @@
-let s1 = '2 + 2'              // creates a string primitive
-let s2 = new String('2 + 2')  // creates a String object
-console.log(eval(s1))         // returns the number 4
-console.log(eval(s2))
-console.log(eval(s2.valueOf()))  // returns the number 4
+let firstName = "Carlos";
+let lastName = "Stevenson";
+let birthYear = 1948;
+let thisYear = 1965;
+let fullName = firstName + lastName;
+let age = thisYear - birthYear;
+
+const greeting = "Hello! My name is " + firstName + " " + lastName + "and I am " + age + " years old."
+
+console.log(greeting);
