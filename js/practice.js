266087e559e5208db9c493a25503a08c61d57166
sockmonkeyy <mr.richardson718@gmail.com>
2022-09-27 10:57:19 -0500

    added on scroll animation folder

--- js/practice.js
+++ js/practice.js
@@ -1,18 +1,8 @@
+const cats = ['Leopard', 'Serval', 'Jaguar', 'Tiger', 'Caracal', 'Lion'];
 
-
-let greeting = 'Greetings' 
-let i = 0;
-let text = ''
-
-while (text[i]) {
-   
-    //for (let i = 0; i < greeting.length; i++)
-    text += greeting[i];
-    i++;
-    console.log(text);
-   
+for (const cat of cats) {
+  console.log(cat);
 }
-
    
 
 
